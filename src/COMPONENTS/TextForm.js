d98e9c17a6a4b1b10f077b08008b69bc9c4f74d9
abhigit01 <abhisheknke717@gmail.com>
2024-04-15 12:12:46 +0530

    add btn

--- src/COMPONENTS/TextForm.js
+++ src/COMPONENTS/TextForm.js
@@ -1,43 +1,64 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const handleUpClick = ()=>{
+    const handleUpClick = () => {
         // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText)
     }
-    const handleLowClick = ()=>{
+    const handleLowClick = () => {
         // console.log("Uppercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText)
     }
+    const clearText = () => {
+        let newText = '';
+        setText(newText)
+    }
+    const [countWord, setCountWord] = useState(0);
 
-
-    const handleOnChange = (event)=>{
+    const handleOnChange = (event) => {
         // console.log("on change");
         setText(event.target.value);
-        // text = event.target.value;
+        console.log(text, text.length)
+        // text = event.target.value
     }
+    // const textCount = () => {
+    //     if (text == "") {
+    //         let a = setCountWord(0);
+    //         return(a)
+    //     }
+    //     else {
+    //         let b = setCountWord(text.split(" ").length)
+    //         return(b)
+    //     }
+    // }
+
+
+
     const [text, setText] = useState('');
     return (
         <>
-        <div className='container'>
+            <div className='container'>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                     <textarea className="form-control" placeholder='enter the text here to convert' value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
-                     
+
                 </div >
                 <div>
-                <button className="btn btn-primary mx-3 my-3" onClick={handleUpClick}> Convet Text to Upper_Case</button>
-                <button className="btn btn-primary mx-3 my-3" onClick={handleLowClick}> Convet Text to lower_Case</button>
+                    <button className="btn btn-primary mx-3 my-3" onClick={handleUpClick}> Convet Text to Upper_Case</button>
+                    <button className="btn btn-primary mx-3 my-3" onClick={handleLowClick}> Convet Text to Lower_Case</button>
+                    <button className="btn btn-primary mx-3 my-3" onClick={clearText}>Clear Text</button>
                 </div>
-        </div>
-        <div className="container my-3">
-            <h1>
-                Your Text Summary
-            </h1>
-            <p> {text.split(" ").length} Words and {text.length} Characters</p>
-        </div>
+                
+            </div>
+            <div className="container my-3">
+                <h1>
+                    Your Text Summary
+                </h1>
+                <p> {text.split(' ').length} Words and {text.length} Characters</p>
+
+            </div>
         </>
     )
 }
